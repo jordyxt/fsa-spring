c942a0a94cda2e7de7905f04762eb3043148de93
Jordy Arrese <jordyarresehuaman@gmail.com>
2022-05-15 22:11:05 +0200

    Added login tests #6

--- src/test/java/es/tfm/fsa/infraestructure/api/resources/UserResourceIT.java
+++ src/test/java/es/tfm/fsa/infraestructure/api/resources/UserResourceIT.java
@@ -7,11 +7,15 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 import static es.tfm.fsa.infraestructure.api.resources.UserResource.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RestTestConfig
 public class UserResourceIT {
     @Autowired
     private WebTestClient webTestClient;
+    @Autowired
+    private RestClientTestService restClientTestService;
+
     @Test
     void testCreateUser() {
         this.webTestClient.post().uri(uriBuilder -> uriBuilder
@@ -19,5 +23,15 @@ public class UserResourceIT {
                 .build()).
                 body(Mono.just(UserDto.builder().username("tester2").email("e1").password("12345").build()), UserDto.class).
                 exchange().expectStatus().isOk();
+    }
+    @Test
+    void testLoginAdmin() {
+        this.restClientTestService.loginAdmin(this.webTestClient);
+        assertTrue(this.restClientTestService.getToken().length() > 10);
+    }
+    @Test
+    void testLoginBasic() {
+        this.restClientTestService.loginBasic(this.webTestClient);
+        assertTrue(this.restClientTestService.getToken().length() > 10);
     }
 }
